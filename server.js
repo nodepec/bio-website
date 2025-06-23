af95781200600eaa491dabf8f9a98b65ce6fc578
yensen <167006426+nodepec@users.noreply.github.com>
2025-06-23 20:57:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,20 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the main HTML file on the root route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Handle 404 errors with a custom page
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
